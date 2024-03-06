4174beda92920310ded7fea5ee168b25623e4c17
Encheng Zhou <ezhou@c3r1s3.42malaga.com>
2024-03-06 15:59:37 +0100

    Add test struct, libft.a

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rguerrer <rguerrer@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: ezhou <ezhou@student.42malaga.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 12:42:47 by rguerrer          #+#    #+#             */
-/*   Updated: 2024/02/28 12:42:47 by rguerrer         ###   ########.fr       */
+/*   Updated: 2024/03/06 15:49:58 by ezhou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,31 @@
 # define MINISHELL_H
 
 # include "./libft/libft.h"
+# include <limits.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <limits.h>
 
 # define ERROR 1
 # define SUCCESS 0
 
 typedef struct s_env
 {
-	char	**env;
-	int		env_size;
-}				t_env;
+	char			**env;
+	int				env_size;
+}					t_env;
 
 typedef struct s_cmd
 {
 	struct s_cmd	*next;
-	//t_fds           pipes;
+	// t_fds           pipes;
 	char			*cmd_path;
 	char			**arg;
-    t_env            **env;
+	t_env			*env;
 	int				num_arg;
-}							t_cmd;
+}					t_cmd;
 
-int	main(int argc, char **argv, char **envp);
+int					main(int argc, char **argv, char **envp);
+int	ft_pwd(void);
 
 #endif
