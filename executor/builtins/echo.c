048e4370601bb3dd8c249b25f7781b6aa15b0193
rguerrer23 <ricardoguerrerocabrera@gmail.com>
2024-06-18 17:07:04 +0200

    ...

--- executor/builtins/echo.c
+++ executor/builtins/echo.c
@@ -1,15 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lexer.c                                            :+:      :+:    :+:   */
+/*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rguerrer <rguerrer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/02 12:24:27 by rguerrer          #+#    #+#             */
-/*   Updated: 2024/06/18 16:39:43 by rguerrer         ###   ########.fr       */
+/*   Created: 2024/06/18 16:52:32 by rguerrer          #+#    #+#             */
+/*   Updated: 2024/06/18 17:03:50 by rguerrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
+int ft_echo(char **args)
+{
+    int i;
 
+    i = 1;
+    while (args[i])
+    {
+        ft_putstr_fd(args[i], STDOUT_FILENO);
+        if (args[i + 1])
+            ft_putstr_fd(" ", STDOUT_FILENO);
+        i++;
+    }
+    ft_putstr_fd("\n", STDOUT_FILENO);
+    return (EXIT_SUCCESS);
+}
--- mini.h
+++ mini.h
@@ -6,7 +6,7 @@
 /*   By: rguerrer <rguerrer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 12:51:36 by rguerrer          #+#    #+#             */
-/*   Updated: 2024/06/18 16:40:38 by rguerrer         ###   ########.fr       */
+/*   Updated: 2024/06/18 17:05:10 by rguerrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@
 # define EXIT_SUCCESS 0
 # define EXIT_FAILURE 1
 
+# define STDIN_FILENO 0
+# define STDOUT_FILENO 1
+# define STDERR_FILENO 2
+
 
 # define SHELL_NAME "minishell"
 # define SHELL_PROMPT "minishell$ "
@@ -37,5 +41,6 @@
 
 
 int		main(int argc, char **argv, char **envp);
+int     ft_echo(char **args);
 
 #endif
